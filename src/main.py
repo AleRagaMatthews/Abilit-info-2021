f828be81d3af2be0b6d3b78c2ccf164b6951f664
AleRagaMatthews <95907783+AleRagaMatthews@users.noreply.github.com>
2021-12-10 10:42:10 +0000

    src/

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import sys
+import math
+
+a, b=10,21
+c=2.32442
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+
+
+print("a={0},b={1}, {2}, {3}, {4}, {5}".format(a+b, a-b, a*b, b/a, b%a, a**b))
